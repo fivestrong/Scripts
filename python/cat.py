b230a03f811202de6764abbcb2b9b3516444fbb8
fivestrong <clarencewuq@gmail.com>
2017-09-19 13:06:12 +0800

    add '-n' choice and improve the code

--- python/cat.py
+++ python/cat.py
@@ -2,12 +2,6 @@
 # _*_ coding:utf-8 _*_
 import argparse
 
-def readfile(filename):
-    '''print a file to standard output'''
-    with open(filename) as f:
-        file_data = f.read()
-    return file_data
-
 def get_args():
     parser = argparse.ArgumentParser(
         description="A simple python script for cat command",
@@ -15,12 +9,22 @@ def get_args():
     )
 
     # required argument 
-    parser.add_argument("filenames", nargs='*', help="the filename that you want to display")  
+    parser.add_argument(dest="filenames", metavar='filename', type=str, nargs='*', help="the filename that you want to display")
+    parser.add_argument('-n', '--numbers', action='store_true', help='umber all output lines')
     return parser.parse_args()
 
 if __name__ == '__main__':
-   files = get_args().filenames
-   content = ''
-   for filename in files:
-       content = content + readfile(filename)
-   print(content)
+   args = get_args()
+   line_number = 1
+#    print(">>> parsed args: ", args)
+   for filename in args.filenames:
+       with open(filename) as f:
+            if args.numbers:
+               for line in f.readlines():
+                   print(f"\t{line_number}\t{line}", end="")
+                   line_number += 1
+            else:
+                print(f.read())
+
+
+               
